5aaf6666cdfa9ddb73beb7edceaafac02409260b
victor-ekshtut <96315959+victor-ekshtut@users.noreply.github.com>
2021-12-17 21:53:39 -0500

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package com.victor.helloworld;
+
+public class HelloWorld {
+
+  public static void main(final String[] args) {
+    System.out.println("Hello, World! The current time is " + System.getTime());
+  }
+
+}
