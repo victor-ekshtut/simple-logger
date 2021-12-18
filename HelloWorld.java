4ff72f2a50869e27077bca1bfb8a39507efd04a0
victor-ekshtut <96315959+victor-ekshtut@users.noreply.github.com>
2021-12-17 21:59:28 -0500

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.victor.helloworld;
-
-public class HelloWorld {
-
-  public static void main(final String[] args) {
-    System.out.println("Hello, World! The current time is " + System.getTime());
-  }
-
-}
