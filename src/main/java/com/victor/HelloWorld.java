9f2ccdc0c6ceee97135583f8b2e737b7b529b657
victor-ekshtut <96315959+victor-ekshtut@users.noreply.github.com>
2021-12-21 15:32:13 -0500

    Use old log4j with vulnerabilities

--- src/main/java/com/victor/HelloWorld.java
+++ src/main/java/com/victor/HelloWorld.java
@@ -1,9 +1,15 @@
 package com.victor;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
+
 public class HelloWorld {
 
   public static void main(final String[] args) {
+    Logger logger = Logger.getLogger("test");
+    
     System.out.println("Hello, World! The current time is test " + System.currentTimeMillis());
+    logger.log(Priority.DEBUG, "Writing to logger");
   }
 
 }
