6fb45e03d2938d54a1c48c14c40f1ac185e6bd36
victor-ekshtut <96315959+victor-ekshtut@users.noreply.github.com>
2021-12-21 15:16:00 -0500

    Commit from local

--- src/main/java/com/victor/HelloWorld.java
+++ src/main/java/com/victor/HelloWorld.java
@@ -3,7 +3,7 @@ package com.victor;
 public class HelloWorld {
 
   public static void main(final String[] args) {
-    System.out.println("Hello, World! The current time is " + System.currentTimeMillis());
+    System.out.println("Hello, World! The current time is test " + System.currentTimeMillis());
   }
 
 }
