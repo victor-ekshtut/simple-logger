d9c7f5396ff50c08e8d5158e55eafe5de6d615c9
victor-ekshtut <96315959+victor-ekshtut@users.noreply.github.com>
2021-12-21 13:23:00 -0500

    Update HelloWorld.java

--- src/main/java/com/victor/HelloWorld.java
+++ src/main/java/com/victor/HelloWorld.java
@@ -3,7 +3,7 @@ package com.victor.helloworld;
 public class HelloWorld {
 
   public static void main(final String[] args) {
-    System.out.println("Hello, World! The current time is " + System.getTime());
+    System.out.println("Hello, World! The current time is " + System.currentTimeMillis());
   }
 
 }
