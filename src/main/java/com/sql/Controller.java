db9930d5b9c1a14cec11a98ef267095c7e4274fa
victor-ekshtut <96315959+victor-ekshtut@users.noreply.github.com>
2021-12-21 16:17:52 -0500

    Update Controller.java

--- src/main/java/com/sql/Controller.java
+++ src/main/java/com/sql/Controller.java
@@ -1,5 +1,24 @@
 package com.sql;
 
-public class Controller {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+public class Controller {
+  
+  public void unsafeFindAccountsByCustomerId(String customerId) throws SQLException {
+    DataSource dataSource = null;
+    
+    String sql = "select "
+      + "customer_id,acc_number,branch_id,balance "
+      + "from Accounts where customer_id = '"
+      + customerId 
+      + "'";
+    @SuppressWarnings("null")
+    Connection c = dataSource.getConnection();
+    @SuppressWarnings("unused")
+    ResultSet rs = c.createStatement().executeQuery(sql);
+}
 }
